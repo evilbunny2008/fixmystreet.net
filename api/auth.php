56bfd1a3911836959b569cfe5c8ea9cf7f463718
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2021-03-19 15:27:19 +1100

    more tweaks

--- api/auth.php
+++ api/auth.php
@@ -2,9 +2,10 @@
 	require_once('../mysql.php');
 
 	$email = mysqli_real_escape_string($link, strip_tags(trim($_GET['email'])));
-	$password = mysqli_real_escape_string($link, strip_tags(trim($_GET['password'])));
+	$password = password_hash(strip_tags(trim($_GET['password'])) + $secret, PASSWORD_ARGON2ID);
+	$password = mysqli_real_escape_string($link, $password);
 
-	$query = "select `id` from `users` where `email`='$email' and `password`=PASSWORD('$password')";
+	$query = "select `id` from `users` where `email`='$email' and `password`='$password'";
 	$res = mysqli_query($link, $query);
 	if(mysqli_num_rows($res) == 1)
 	{
--- mysql-example.php
+++ mysql-example.php
@@ -5,5 +5,6 @@
 	$password = "password";
 	$hostname = "localhost";
 	$gapikey  = "go to https://developers.google.com/maps/documentation/places/web-service/autocomplete#place_types to get an api key";
+	$secret   = "private secret goes here";
 
 	$link = new mysqli($hostname, $username, $password, $database);
