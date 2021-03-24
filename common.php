3a176faee1827014461827a2ef63170171ddc599
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2021-03-24 23:36:58 +1100

    geocoding search

--- common.php
+++ common.php
@@ -65,9 +65,9 @@
 		$lng = floatval($lng);
 		if($lat != 0 && $lat >= -90 && $lat <= 90 && $lng != 0 && $lng >= -180 && $lng <= 180)
 		{
-//			$url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&output=json&sensor=true&key=$gapikey";
-//			$address = file_get_contents($url);
-			$address = file_get_contents("data.txt");
+			$url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&output=json&sensor=true&key=$gapikey";
+			$address = file_get_contents($url);
+//			$address = file_get_contents("data.txt");
 			$json_data = json_decode($address, true);
 
 			if($json_data['status'] == "OK")
