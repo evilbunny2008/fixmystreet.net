b3280f10068f6c4b16959367ea88432139d12c9d
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-03-31 14:51:18 +0530

    testing close

--- js/index.js
+++ js/index.js
@@ -7,6 +7,9 @@ const divs = ["step-one", "step-two", "step-three"];
 	for (let i = 0; i < step.length-1; i++) {
 			step[i].addEventListener("click", function() {
 			step[i].nextElementSibling.removeAttribute("hidden");
+			if(!step[i].nextElementSibling.hasAttribute("hidden")) {
+				step[i].setAttribute("hidden", "");
+			}
 			for(let j=0; j<divs.length; j++) {
 				if(step[i].nextElementSibling.id != divs[j]) {
 					if(!document.getElementById(divs[j]).hasAttribute("hidden")) {
