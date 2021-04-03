1f36dc72e22168e3b0526e6f35cf0eae035ee38b
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2021-04-03 23:11:10 +1100

    tweaks

--- map.php
+++ map.php
@@ -158,7 +158,7 @@
 
           <ul class="pure-menu-list">
             <li class="pure-menu-item">
-            <form action="<?= $_SERVER['PHP_SELF']?>" method="post">
+            <form action="<?= $_SERVER['PHP_SELF']?>" method="post" enctype="multipart/form-data">
               <a href="#" class="pure-menu-link">Step 1</a>
               <div id="step-one">
                 <p class="is-center">Drag the marker on the map</p>
