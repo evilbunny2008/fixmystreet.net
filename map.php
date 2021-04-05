2abf9694faab11c5743928b69a32e2f7c976a2c2
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-04-05 16:18:18 +0530

    added a class

--- map.php
+++ map.php
@@ -207,7 +207,8 @@
 						let parent = document.getElementById('reportProblem');
 						if(parent.nextElementSibling.tagName != "DIV")
 						{
-							let reportInfo = document.createElement('div', 'class="reportInfo"');
+							let reportInfo = document.createElement('div');
+              reportInfo.className = "reportInfo";
 							reportInfo.innerHTML = `${bits[3]}`;
 							parent.after(reportInfo);
 						}
