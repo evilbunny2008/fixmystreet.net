77ce6200f2db85ef55fc2686a5c228b6834640a0
evilbunny2008 <34211365+evilbunny2008@users.noreply.github.com>
2021-04-07 11:09:03 +1000

    Tweaks

---
+++ extra.php
@@ -0,0 +1,30 @@
+<?php
+	require_once('common.php');
+
+	header("Content-Type: text/plain");
+
+	$id = intval($_GET['id']);
+	if($id <= 0)
+	{
+		$arr['status'] = "FAIL";
+		$arr['errmsg'] = "Invalid ID number.";
+		echo json_encode($arr);
+		exit;
+	}
+
+	$query = "SELECT * FROM `problem` WHERE `id`=$id";
+	$res = mysqli_query($link, $query);
+	$row = mysqli_fetch_assoc($res);
+	$row['lastupdate'] = date("F j, Y, g:i a", strtotime($row['lastupdate']));
+	$row['created'] = date("F j, Y, g:i a", strtotime($row['created']));
+
+	$row['photos'] = array();
+
+	$query = "SELECT * FROM `photos` WHERE `problem_id`=$id";
+	$res = mysqli_query($link, $query);
+	while($prow = mysqli_fetch_assoc($res))
+		$row['photos'][] = $prow;
+
+	$row['status'] = "OK";
+	echo json_encode($row);
+	exit;
