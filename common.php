5483de911d38e5be5728e5cca8ebb56642a3f6ea
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-05-21 14:16:37 +0530

    added problem_id return

--- common.php
+++ common.php
@@ -383,6 +383,11 @@
 			echo json_encode($arr);
 			exit;
 		}
+		$query  = "INSERT INTO `problem` SET `latitude`=$lat, `longitude`=$lng, `address`='$address', `council`='$council', `summary`='$summary', `user_id`=$userid, ";
+		$query .= "`site_id`=1,`extra`='$extra', `defect_id`=$defect";
+		mysqli_query($link, $query);
+		$problem_id = mysqli_insert_id($link);
+		return $problem_id;
 	}
 
 	$header = '    <div class="flex-wrapper">
