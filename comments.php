53a28d76fd746dbf7755481b33c7b14434e28f8b
V01D0 <38071738+V01D0@users.noreply.github.com>
2021-06-21 23:46:47 +0530

    comments API

---
+++ comments.php
@@ -0,0 +1,30 @@
+<?php
+	require_once('common.php');
+
+        // if(!isset($_SERVER['HTTP_REFERER']) || strtolower(substr($_SERVER['HTTP_REFERER'], 0, strlen($refererurl))) != $refererurl)
+        // {
+        //         $arr['status'] = "FAIL";
+        //         $arr['errmsg'] = "Invalid latitude or longitude";
+        //         echo json_encode($arr);
+        //         exit;
+        // }
+
+	header("Content-Type: text/plain");
+
+        $pid = intval($_REQUEST['pid']);
+
+        // $query = "SELECT * FROM `comment` WHERE `problem_id`=$pid";
+        $query = "SELECT `comment`.*,`users`.`name` FROM `comment`, `users` WHERE `comment`.`problem_id`=$pid AND `comment`.`user_id` = `users`.`id`";
+        $res = mysqli_query($link, $query);
+        if(mysqli_num_rows($res) <= 0)
+        {
+                $arr['status'] = "FAIL";
+                $arr['errmsg'] = "Can't retrieve comments!";  
+                echo json_encode($arr);
+                exit;      
+        }
+        $rows = array();
+        while($row = mysqli_fetch_assoc($res)) {
+                array_push($rows, $row);
+        }
+        echo json_encode($rows);
